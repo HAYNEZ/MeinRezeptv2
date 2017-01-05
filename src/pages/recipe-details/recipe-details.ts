1cea7687df68cf5edb966f41d1066170a3a593fc
VeronikaBertels <Veve.Bertels@web.de>
2017-01-05 16:57:08 +0100

    recipe details frontend

--- src/pages/recipe-details/recipe-details.ts
+++ src/pages/recipe-details/recipe-details.ts
@@ -11,16 +11,37 @@ import { NavController, NavParams } from 'ionic-angular';
   selector: 'page-recipe-details',
   templateUrl: 'recipe-details.html'
 })
+
 export class RecipeDetailsPage {
 
+  section:any;
   recipe: any;
   constructor(public navCtrl: NavController,
               public params: NavParams) {
     this.recipe = params.get("recipe");
+    this.section = "general";
   }
 
-  ionViewDidLoad() {
-    console.log('Hello RecipeDetailsPage Page');
+  switchGeneral() {
+    document.getElementById('demo').innerHTML = this.recipe.time;
+  }
+
+  switchIngredients() {
+    document.getElementById('demo').innerHTML = this.recipe.time;
+  }
+
+  switchSteps() {
+    document.getElementById('demo').innerHTML = 'Steps';
+  }
+
+
+  range(min, max ){
+    let input = [];
+    for( let i = min; i<=max; i++){
+      input.push(i);
+    }
+    console.log(input);
+    return input;
   }
 
 }
