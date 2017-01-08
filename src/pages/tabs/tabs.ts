df47fb27a5ef8ffc3e2e0a60c3a178ea1cbebd9f
VeronikaBertels <Veve.Bertels@web.de>
2017-01-08 18:11:09 +0100

    Refactoring

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
 import { RecipeBookPage } from '../recipe-book/recipe-book';
-//import { AddRecipePage } from '../add-recipe/add-recipe';
 import { ShoppingListPage} from '../shopping-list/shopping-list';
 import { HomePage } from '../home/home';
 
@@ -10,8 +9,6 @@ import { HomePage } from '../home/home';
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
-  // this tells the tabs component which Pages
-  // should be each tab's root Page
   tab1Root: any;
   tab2Root: any;
   tab3Root: any;
