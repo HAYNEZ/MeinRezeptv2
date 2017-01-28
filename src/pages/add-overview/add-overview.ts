b5797b8d9389ac40596b31dc10036008504fa72e
Stefanie Otten <stefanie.otten@taktil.ch>
2017-01-28 15:04:26 +0100

    merge

---
+++ src/pages/add-overview/add-overview.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+import { NavController, NavParams } from 'ionic-angular';
+import { AddRecipeManuallyPage } from '../add-recipe-manually/add-recipe-manually';
+import { TextRecognitionPage } from '../text-recognition/text-recognition';
+
+@Component({
+  selector: 'page-add-overview',
+  templateUrl: 'add-overview.html'
+})
+export class AddOverviewPage {
+  textrecognitionOption: Number = 0;
+
+  constructor(
+    public navCtrl: NavController,
+    public params: NavParams
+  ) {}
+
+  navigateOptionOne() {
+    this.navCtrl.push(AddRecipeManuallyPage);
+  }
+
+  navigateOptionTwo(option: Number){
+    this.navCtrl.push(TextRecognitionPage, {option: option});
+  }
+}
